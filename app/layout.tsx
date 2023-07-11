d408dddf526f576fe061a4d3d6ce79d754d0f8ef
svenhfiedler <sven@shf.dev>
2023-07-11 09:56:53 +0200

    added Header and Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import './globals.css';
+import Header from './header';
+import Footer from './footer';
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
         <body>
+          <Header />
           {children}
+          <Footer/>
         </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next'
 export const metadata: Metadata = {
     title: 'shf.dev Starter',
+    description: 'shf.dev Starter. Next 13.4. TailwindCSS 3.3.2',
   }
 export default function Page() {
     return <h1>shf.dev Starter. Next 13.4. TailwindCSS 3.3.2</h1>
